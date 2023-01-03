12c0ff9a599a19a3051df0e744b8bdfef97581e7
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2023-01-03 00:03:35 -0300

    finishing profile page

--- profile.php
+++ profile.php
@@ -13,35 +13,30 @@ $gameDAO = new GameDAO($conn, $BASE_URL);
 // Taked user id
 $id = filter_input(INPUT_GET, "id");
 
-if(empty($id)) {
+if (empty($id)) {
 
-    if(!empty($userData)) {
+    if (!empty($userData)) {
 
         $id = $userData->id;
-
     } else {
 
         $message->setMessage("The user was not found.", "error", "index.php");
-
     }
-
 } else {
 
     $userData = $userDAO->findById($id);
 
     // If not find user
-    if(!$userData) {
+    if (!$userData) {
 
         $message->setMessage("The user was not found.", "error", "index.php");
-
     }
-
 }
 
 $fullName = $user->getFullName($userData);
 
 if ($userData->image == "") {
-  $userData->image = "user.png";
+    $userData->image = "user.png";
 }
 
 // Games that the user has added
@@ -52,24 +47,26 @@ $userGames = $gameDAO->getGamesByUserId($id);
 <div id="main-container" class="container-fluid">
     <div class="col-md-8 offset-md-2">
         <div class="row profile-container">
-            <div class="col-md-12">
+            <div class="col-md-12 about-container">
                 <h1 class="page-title"><?= $fullName ?></h1>
-                <div id="profile-image-container" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>')"></div>
+                <div id="profile-image-container" class="profile-image" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>')"></div>
                 <h3 class="about-title">About:</h3>
-                <?php if(!empty($userData->bio)): ?>
+                <?php if (!empty($userData->bio)) : ?>
                     <p class="profile-description"><?= $userData->bio ?></p>
-                <?php else: ?>
+                <?php else : ?>
                     <p class="profile-description">The user hasn't written anything here yet...</p>
-                <?php endif; ?>               
+                <?php endif; ?>
             </div>
-            <div class="col-md-12 added-games-container">
+            <div class="col-md-12 add-game-container">
                 <h3>Games adds:</h3>
-                <?php foreach($userGames as $game): ?>
-                    <?php require("templates/game_card.php"); ?>
-                <?php endforeach; ?>
-                <?php if(count($userGames) === 0): ?>
-                    <p class="empty-list">User hasn't uploaded games yet.</p>
-                <?php endif; ?> 
+                <div class="games-container">
+                    <?php foreach ($userGames as $game) : ?>
+                        <?php require("templates/game_card.php"); ?>
+                    <?php endforeach; ?>
+                    <?php if (count($userGames) === 0) : ?>
+                        <p class="empty-list">User hasn't uploaded games yet.</p>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
